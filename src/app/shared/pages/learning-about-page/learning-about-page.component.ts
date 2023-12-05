17bdc7cfb51a2448cad91f95c166ffd7036c2020
Daniel Moso <d.moso.unt@outlook.com>
2023-12-05 00:42:39 -0500

    size video
    size video

--- src/app/shared/pages/learning-about-page/learning-about-page.component.ts
+++ src/app/shared/pages/learning-about-page/learning-about-page.component.ts
@@ -32,7 +32,7 @@ export class LearningAboutPageComponent implements AfterViewInit, OnDestroy, OnI
 
   onResize = (): void => {
     // Automatically expand the video to fit the page up to 1200px x 720px
-    this.videoWidth = Math.min(this.demoYouTubePlayer.nativeElement.clientWidth, 1200);
+    this.videoWidth = Math.min(this.demoYouTubePlayer.nativeElement.clientWidth, 800);
     this.videoHeight = this.videoWidth * 0.6;
     this._changeDetectorRef.detectChanges();
   }
